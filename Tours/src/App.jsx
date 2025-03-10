fb1be6aaa0e11e3a047b86a8a33060ac4efee173
GeNeRaL <abood.v000@email.com>
2025-03-11 00:57:40 +0300

    Finish SetUp

---
+++ Tours/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <section>
+          <header>
+            <div></div>
+          </header>
+        </section>
+
+        <section>
+          <main>
+            <h1>Hello EveryOne</h1>
+          </main>
+        </section>
+
+        <section>
+          <footer></footer>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;
