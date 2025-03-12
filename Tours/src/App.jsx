30d6b150349976c768b38f758251bad42198e659
GeNeRaL <abood.v000@email.com>
2025-03-12 07:50:32 +0300

    finish Header

--- Tours/src/App.jsx
+++ Tours/src/App.jsx
@@ -1,19 +1,41 @@
 import "./App.css";
+import img from "../src/img/pic.webp";
 
 function App() {
   return (
     <>
-      <div>
-        <section>
-          <header>
-            <div></div>
+      <div
+        // className="w-screen"
+        style={{
+          padding: "50px",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <section
+          style={{
+            height: "94vh",
+            background: "orange",
+          }}
+        >
+          <header style={{ background: "red", height: "15vh" }}>
+            <div>
+              <img
+                style={{ height: "100px", width: "100px" }}
+                src={img}
+                alt=""
+              />
+              <button class="w-12 h-12 bg-blue-500 rounded-full flex flex-col justify-center items-center relative group">
+                <span class="w-6 h-1 bg-black mb-1"></span>
+                <span class="w-6 h-1 bg-black mb-1"></span>
+                <span class="w-6 h-1 bg-black"></span>
+              </button>
+            </div>
           </header>
         </section>
 
         <section>
-          <main>
-            <h1>Hello EveryOne</h1>
-          </main>
+          <main></main>
         </section>
 
         <section>
