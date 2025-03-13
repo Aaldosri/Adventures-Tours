ee81c895edc376bd35a979b464c903a7ef8284a6
GeNeRaL <abood.v000@email.com>
2025-03-13 03:35:08 +0300

    x

--- Tours/src/App.jsx
+++ Tours/src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
 import img from "../src/img/pic.webp";
+import img2 from "../src/img/pic3.png";
+import img3 from "../src/img/2.png";
+import "./App.scss";
+
+import { useState } from "react";
 
 function App() {
   return (
@@ -19,17 +24,59 @@ function App() {
           }}
         >
           <header style={{ background: "red", height: "15vh" }}>
-            <div>
+            <div
+              style={{ background: "green" }}
+              className="flex justify-between items-center p-4"
+            >
               <img
-                style={{ height: "100px", width: "100px" }}
-                src={img}
+                style={{ height: "150px", width: "150px" }}
+                src={img3}
                 alt=""
               />
-              <button class="w-12 h-12 bg-blue-500 rounded-full flex flex-col justify-center items-center relative group">
-                <span class="w-6 h-1 bg-black mb-1"></span>
-                <span class="w-6 h-1 bg-black mb-1"></span>
-                <span class="w-6 h-1 bg-black"></span>
-              </button>
+
+              <div class="navigation">
+                <input
+                  type="checkbox"
+                  class="navigation__checkbox"
+                  id="navi-toggle"
+                ></input>
+
+                <label for="navi-toggle" class="navigation__button">
+                  <span class="navigation__icon">&nbsp;</span>
+                </label>
+
+                <div class="navigation__background">&nbsp;</div>
+
+                <nav class="navigation__nav">
+                  <ul class="navigation__list">
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link">
+                        <span>01</span>About Us
+                      </a>
+                    </li>
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link">
+                        <span>02</span>Your benfits
+                      </a>
+                    </li>
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link">
+                        <span>03</span>Popular tours
+                      </a>
+                    </li>
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link">
+                        <span>04</span>Stories
+                      </a>
+                    </li>
+                    <li class="navigation__item">
+                      <a href="#" class="navigation__link">
+                        <span>05</span>Book now
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
           </header>
         </section>
