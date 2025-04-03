dbd3e58e7941354d16d60341170d97066c78aa51
GeNeRaL <abood.v000@email.com>
2025-04-03 05:34:27 +0300

    Finish

--- Tours/src/App.jsx
+++ Tours/src/App.jsx
@@ -546,91 +546,91 @@ function App() {
               {/* ===== END BOOK ===== */}
             </main>
           </section>
+        </div>
 
-          {/* ===== FOOTER ===== */}
+        {/* ===== FOOTER ===== */}
 
-          <section>
-            <footer className="site-footer">
-              <div className="footer-container">
-                <div className="footer-columns">
-                  <div className="footer-column">
-                    <h3 className="footer-column-title">
-                      <i data-feather="shopping-bag"></i>{" "}
-                      <span className="span">Contact Us</span>
-                    </h3>
-                    <nav className="footer-nav">
-                      <ul className="footer-nav-list">
-                        <li className="footer-nav-item">
-                          <a href="" className="span-color">
-                            <span className="span-color">Contact</span>
-                          </a>
-                        </li>
-                        <li className="footer-nav-item">
-                          <a href="" className="span-color">
-                            <span className="span-color">Us</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
-                  <div className="footer-column">
-                    <h3 className="footer-column-title">
-                      <i data-feather="share-2"></i>{" "}
-                      <span className="span">Social Media</span>
-                    </h3>
-                    <nav className="footer-nav">
-                      <ul className="footer-nav-list">
-                        <li className="footer-nav-item">
-                          <a href="" className="footer-nav-link">
-                            <i data-feather="youtube"></i>{" "}
-                            <span className="span-color">Youtube</span>
-                          </a>
-                        </li>
-                        <li className="footer-nav-item">
-                          <a href="" className="footer-nav-link">
-                            <i data-feather="facebook"></i>{" "}
-                            <span className="span-color">Facebook</span>
-                          </a>
-                        </li>
-                        <li className="footer-nav-item">
-                          <a href="" className="footer-nav-link">
-                            <i data-feather="instagram"></i>{" "}
-                            <span className="span-color">Instagram</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
-                  <div className="footer-column">
-                    <h3 className="footer-column-title">
-                      <i data-feather="send"></i>{" "}
-                      <span className="span">Privacy Policy</span>
-                    </h3>
-                    <nav className="footer-nav">
-                      <ul className="footer-nav-list">
-                        <li className="footer-nav-item">
-                          <a
-                            href="mailto:contact.laboiserie@gmail.com"
-                            className="footer-nav-link"
-                          >
-                            <span className="span-color"> contact@.com</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
+        <section style={{ paddingBottom: "50px" }}>
+          <footer className="site-footer">
+            <div className="footer-container">
+              <div className="footer-columns">
+                <div className="footer-column">
+                  <h3 className="footer-column-title">
+                    <i data-feather="shopping-bag"></i>{" "}
+                    <span className="span">Contact Us</span>
+                  </h3>
+                  <nav className="footer-nav">
+                    <ul className="footer-nav-list">
+                      <li className="footer-nav-item">
+                        <a href="" className="span-color">
+                          <span className="span-color">Contact</span>
+                        </a>
+                      </li>
+                      <li className="footer-nav-item">
+                        <a href="" className="span-color">
+                          <span className="span-color">Us</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
                 </div>
-
-                <div className="footer-copyrights">
-                  <img src={img3} alt="Logo" />
-                  <h2>Copy Right @Abdullah 2025</h2>
+                <div className="footer-column">
+                  <h3 className="footer-column-title">
+                    <i data-feather="share-2"></i>{" "}
+                    <span className="span">Social Media</span>
+                  </h3>
+                  <nav className="footer-nav">
+                    <ul className="footer-nav-list">
+                      <li className="footer-nav-item">
+                        <a href="" className="footer-nav-link">
+                          <i data-feather="youtube"></i>{" "}
+                          <span className="span-color">Youtube</span>
+                        </a>
+                      </li>
+                      <li className="footer-nav-item">
+                        <a href="" className="footer-nav-link">
+                          <i data-feather="facebook"></i>{" "}
+                          <span className="span-color">Facebook</span>
+                        </a>
+                      </li>
+                      <li className="footer-nav-item">
+                        <a href="" className="footer-nav-link">
+                          <i data-feather="instagram"></i>{" "}
+                          <span className="span-color">Instagram</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="footer-column">
+                  <h3 className="footer-column-title">
+                    <i data-feather="send"></i>{" "}
+                    <span className="span">Privacy Policy</span>
+                  </h3>
+                  <nav className="footer-nav">
+                    <ul className="footer-nav-list">
+                      <li className="footer-nav-item">
+                        <a
+                          href="mailto:contact.laboiserie@gmail.com"
+                          className="footer-nav-link"
+                        >
+                          <span className="span-color"> contact@.com</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
                 </div>
               </div>
-            </footer>
-          </section>
 
-          {/* ===== END FOOTER ===== */}
-        </div>
+              <div className="footer-copyrights">
+                <img src={img3} alt="Logo" />
+                <h2>Copy Right @Abdullah 2025</h2>
+              </div>
+            </div>
+          </footer>
+        </section>
+
+        {/* ===== END FOOTER ===== */}
       </div>
     </>
   );
